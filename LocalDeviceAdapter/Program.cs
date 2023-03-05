e3b84a5515087ca40162f8b78f2dc50c4a5a38a1
Vladimir <riuson@gmail.com>
2023-03-05 10:32:56 +0500

    Reformat

--- LocalDeviceAdapter/Program.cs
+++ LocalDeviceAdapter/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace LocalDeviceAdapter
@@ -9,10 +6,10 @@ namespace LocalDeviceAdapter
     internal static class Program
     {
         /// <summary>
-        /// Главная точка входа для приложения.
+        ///     Главная точка входа для приложения.
         /// </summary>
         [STAThread]
-        static void Main()
+        private static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
