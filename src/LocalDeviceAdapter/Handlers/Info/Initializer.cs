b05e93b8d967a6676ad635f5a94496d35698b858
Vladimir <riuson@gmail.com>
2023-03-06 01:44:51 +0500

    Add version info

---
+++ src/LocalDeviceAdapter/Handlers/Info/Initializer.cs
@@ -0,0 +1,17 @@
+﻿using WebSocketSharp.Server;
+
+namespace LocalDeviceAdapter.Handlers.Info
+{
+    internal class Initializer : IHandlerInitializer
+    {
+        public void Initialize(WebSocketServer server)
+        {
+            server.AddWebSocketService<Handler>("/Info");
+        }
+
+        public void DeInitialize(WebSocketServer server)
+        {
+            server.RemoveWebSocketService("/Info");
+        }
+    }
+}
