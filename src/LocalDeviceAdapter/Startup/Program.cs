2115d2948ac5d778312bc9d078a43805f22190b8
Vladimir <riuson@gmail.com>
2023-03-06 17:41:13 +0500

    Add logger

--- src/LocalDeviceAdapter/Startup/Program.cs
+++ src/LocalDeviceAdapter/Startup/Program.cs
@@ -1,5 +1,8 @@
 using System.Reflection;
 using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace LocalDeviceAdapter
 {
@@ -7,10 +10,26 @@ namespace LocalDeviceAdapter
     {
         private static IContainer CompositionRoot()
         {
-            var builder = new ContainerBuilder();
-            builder.RegisterType<Application>();
-            builder.RegisterAssemblyModules(Assembly.GetExecutingAssembly());
-            return builder.Build();
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterType<Application>();
+            containerBuilder.RegisterAssemblyModules(Assembly.GetExecutingAssembly());
+
+            containerBuilder.RegisterType<LoggerFactory>()
+                .As<ILoggerFactory>()
+                .SingleInstance();
+            containerBuilder.RegisterGeneric(typeof(Logger<>))
+                .As(typeof(ILogger<>))
+                .SingleInstance();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging(loggingbuilder => loggingbuilder
+                .AddConsole()
+                .AddDebug()
+                .SetMinimumLevel(LogLevel.Debug));
+
+            containerBuilder.Populate(serviceCollection);
+
+            return containerBuilder.Build();
         }
 
         public static void Main() //Main entry point
